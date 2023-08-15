a29c00cbb5a0ba5d76cd57f52cca2549301f1a4f
Jan-Niklas Pippert <jan-niklas.pippert@online.de>
2023-08-15 17:04:44 +0200

    new package name

--- astrostreampy/argutils.py
+++ astrostreampy/argutils.py
@@ -1,16 +1,20 @@
-import numpy as np
 import os
+
+import numpy as np
 from astropy.io import fits
+
 
 def add_psf_head(val):
     for file in os.listdir("."):
-        if file.endswith(".fits") and file.startswith(("TS","field")):
-            fits.setval(file,'PSF',value=val)
-            
-def handle_steps(arg : str):
+        if file.endswith(".fits") and file.startswith(("TS", "field")):
+            fits.setval(file, "PSF", value=val)
+
+
+def handle_steps(arg: str):
     return np.array(arg.split(","), dtype=int)
 
-def handle_infile(arg : str):
+
+def handle_infile(arg: str):
     if arg == None:
         raise ValueError("infile needed")
     else:
@@ -18,30 +22,30 @@ def handle_infile(arg : str):
         arg.close
     return infile
 
-def handle_fileargs(arg : str):
 
+def handle_fileargs(arg: str):
     if not isinstance(arg, str):
         l = []
         return l
-    
+
     l = arg.split(",")
     if arg.endswith(","):
         l.pop()
     return l
 
-def handle_colormask_arg(arg : str, cf_num : int):
 
-    l = [] 
+def handle_colormask_arg(arg: str, cf_num: int):
+    l = []
 
     if not isinstance(arg, str):
         for _ in range(cf_num):
             l.append([])
         return l
-    
+
     l = arg.split(",")
 
-    for i,val in enumerate(l):
-        val = val.replace(":",",")
+    for i, val in enumerate(l):
+        val = val.replace(":", ",")
         if val == "":
             l[i] = []
             continue
@@ -50,17 +54,10 @@ def handle_colormask_arg(arg : str, cf_num : int):
 
     return l
 
-def handel_num_pair(arg : str) -> list:
+
+def handel_num_pair(arg: str) -> list:
     l = arg.split(",")
     if arg.endswith(","):
-        #TODO good error message
+        # TODO good error message
         raise ValueError
     return list(map(int, l))
-
-
-
-    
-
-    
-
-    
