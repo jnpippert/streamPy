1600f0d7ebc76fc2f4380b49f15a9ba378748206
jnpippert <pippertjanniklas@gmail.com>
2024-02-25 00:46:51 +0100

    added docstrings

--- astrostreampy/argutils.py
+++ astrostreampy/argutils.py
@@ -1,3 +1,5 @@
+"""Provides convenience methods to handle parsed arguments."""
+
 import numpy as np
 
 
@@ -5,6 +7,11 @@ def handle_steps(arg: str) -> np.ndarray:
     """
     Handles the -s (--step) argument.
     Transform the argument into an np.ndarray.
+
+    Parameters
+    ----------
+    arg : str
+        A string argument.
     """
     return np.array(arg.split(","), dtype=int)
 
@@ -13,8 +20,13 @@ def handle_infile(arg: object) -> str:
     """
     Handles the INFILE argument.
     Transforms the argument into a string.
+
+    Parameters
+    ----------
+    arg : str
+        A object argument.
     """
-    if arg == None:
+    if arg is None:
         raise ValueError("infile needed")
     else:
         infile = arg.name
@@ -26,6 +38,11 @@ def handle_fileargs(arg: str) -> list:
     """
     Handles the masks and interpolation masks arguments -mf and -imf.
     Transfroms the string into a list.
+
+    Parameters
+    ----------
+    arg : str
+        A string argument.
     """
     if not isinstance(arg, str):
         l = []
